389166fd586145581a0ea946658efa4528722856
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-10-13 20:43:09 +0900

    :tada:[Add] [Doit_오라클] 9장 SQL 연습

---
+++ Database/Do it! 오라클로 배우는 데이터베이스 입문/2. 실무에서 가장 많이 사용하는 SQL, 조회/9장.sql
@@ -0,0 +1,151 @@
+SELECT SAL
+FROM EMP
+WHERE ENAME = 'JONES';
+
+SELECT *
+FROM EMP
+WHERE SAL > 2975;
+
+SELECT *
+FROM EMP
+WHERE SAL > (SELECT SAL
+            FROM EMP
+            WHERE ENAME = 'JONES'
+            ORDER BY SAL DESC);
+            
+SELECT *
+FROM EMP
+WHERE HIREDATE < (SELECT HIREDATE
+                    FROM EMP
+                    WHERE ENAME = 'JONES');
+                    
+SELECT E.EMPNO, E.ENAME, E.JOB, E.SAL, D.DEPTNO, D.DNAME, D.LOC
+FROM EMP E, DEPT D
+WHERE E.DEPTNO = D.DEPTNO
+    AND E.DEPTNO = 20
+    AND E.SAL > (SELECT AVG(SAL)
+                    FROM EMP);
+                    
+SELECT *
+FROM EMP
+WHERE DEPTNO IN (20, 30);
+
+SELECT *
+FROM EMP
+WHERE SAL IN (SELECT MAX(SAL)
+                FROM EMP
+                GROUP BY DEPTNO);
+                
+SELECT *
+FROM EMP
+WHERE SAL = ANY (SELECT MAX(SAL)
+                    FROM EMP
+                    GROUP BY DEPTNO);
+                    
+SELECT *
+FROM EMP
+WHERE SAL = SOME (SELECT MAX(SAL)
+                    FROM EMP
+                    GROUP BY DEPTNO);
+                    
+-- 둘이 같음
+
+SELECT *
+FROM EMP
+WHERE SAL < ANY (SELECT SAL
+                    FROM EMP
+                    WHERE DEPTNO = 30)
+ORDER BY SAL, EMPNO;
+
+SELECT *
+FROM EMP
+WHERE SAL < (SELECT MAX(SAL)
+                    FROM EMP
+                    WHERE DEPTNO = 30)
+ORDER BY SAL, EMPNO;
+
+---
+
+SELECT *
+FROM EMP
+WHERE SAL > ANY (SELECT SAL
+                    FROM EMP
+                    WHERE DEPTNO = 30)
+ORDER BY SAL, EMPNO;
+
+SELECT *
+FROM EMP
+WHERE SAL > (SELECT MIN(SAL)
+                    FROM EMP
+                    WHERE DEPTNO = 30)
+ORDER BY SAL, EMPNO;
+
+---
+
+SELECT *
+FROM EMP
+WHERE SAL < ALL (SELECT SAL
+                FROM EMP
+                WHERE DEPTNO = 30);
+                
+SELECT *
+FROM EMP
+WHERE SAL > ALL (SELECT SAL
+                FROM EMP
+                WHERE DEPTNO = 30);
+                
+SELECT *
+FROM EMP
+WHERE EXISTS (SELECT DNAME
+            FROM DEPT
+            WHERE DEPTNO = 10);
+               
+SELECT *
+FROM EMP
+WHERE EXISTS (SELECT DNAME
+            FROM DEPT
+            WHERE DEPTNO = 50);
+           
+SELECT *
+FROM EMP
+WHERE (DEPTNO, SAL) IN (SELECT DEPTNO, MAX(SAL)
+                        FROM EMP
+                        GROUP BY DEPTNO);
+                        
+SELECT E10.EMPNO, E10.ENAME, E10.DEPTNO, D.DNAME, D.LOC
+FROM (SELECT * FROM EMP WHERE DEPTNO = 10) E10,
+    (SELECT * FROM DEPT) D
+WHERE E10.DEPTNO = D.DEPTNO;
+
+WITH
+E10 AS (SELECT * FROM EMP WHERE DEPTNO = 10),
+D AS (SELECT * FROM DEPT)
+
+SELECT E10.EMPNO, E10.ENAME, E10.DEPTNO, D.DNAME, D.LOC
+FROM E10, D
+WHERE E10.DEPTNO = D.DEPTNO;
+
+SELECT *
+FROM EMP E1
+WHERE SAL > (SELECT MIN(SAL)
+            FROM EMP E2
+            WHERE E2.DEPTNO = E1.DEPTNO);
+            
+---
+
+SELECT EMPNO, ENAME, JOB, SAL,
+    (SELECT GRADE
+    FROM SALGRADE
+    WHERE E.SAL BETWEEN LOSAL AND HISAL)AS SALGRADE,
+    DEPTNO,
+    (SELECT DNAME
+    FROM DEPT
+    WHERE E.DEPTNO = DEPT.DEPTNO) AS DNAME
+FROM EMP E;
+
+SELECT EMPNO, ENAME, JOB, SAL,
+    DEPTNO
+FROM EMP E;
+
+SELECT *
+FROM DEPT
